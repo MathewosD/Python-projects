3ce4d80f9c444ba965c770d7904a9dc20285b563
MathewosD <mathewosdawit2025@gmail.com>
2025-11-03 12:36:12 +0300

    Create Python exercises.py

---
+++ Python exercises.py
@@ -0,0 +1,11 @@
+print("Hello!")
+n=int(input("Give me a number."))
+u1=input("What do you want to convert from?(meter or kilometer)")
+if u1=="meter":
+  print(n/1000,"km")
+elif u1=="kilometer":
+  print(n*1000,"m")
+
+
+    
+                                           
