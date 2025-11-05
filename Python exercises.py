f5acab4d62e0dec806c534301c962bd4970e8e78
MathewosD <mathewosdawit2025@gmail.com>
2025-11-05 17:07:53 +0300

    Update Python exercises.py

--- Python exercises.py
+++ Python exercises.py
@@ -1,11 +1,14 @@
 print("Hello!")
-n=int(input("Give me a number."))
-u1=input("What do you want to convert from?(celsius or fahrenheit)")
-if u1=="celsius":
-  print((n-32)*5/9,"degrees fahrenheit")
-elif u1=="fahrenheit":
-  print(n*9/5+32,"degrees celsius")
+def tempchange():
+ n=int(input("Give me a number."))
+ u1=input("What do you want to convert from?(celsius or fahrenheit)")
+ if u1=="celsius":
+  nf=((n-32)*5/9,"degrees fahrenheit")
+  print(nf)
+ elif u1=="fahrenheit":
+  nf=(n*9/5+32,"degrees celsius")
+  print(nf)
+def ()
 
 
-    
                                            
