3fa683ceb30106e401b73a333550e17ed0be1597
MathewosD <mathewosdawit2025@gmail.com>
2025-11-07 10:08:53 +0300

    Update Python exercises.py

--- Python exercises.py
+++ Python exercises.py
@@ -8,7 +8,21 @@ def tempchange():
  elif u1=="fahrenheit":
   nf=(n*9/5+32,"degrees celsius")
   print(nf)
-def ()
+def lengthchange():
+ n=int(input("Give me a number"))
+ u1=input("What is the unit?(m or ft)?")
+ if u1=="m":
+  uf=(u1*3.28,"ft")
+  print(uf)
+ elif u1=="ft":
+  uf=(u1/3.28,"m")
+  print(uf)
+c=input("What do you want to convert Celsius-Fahrenheit(press 1) or meter-feet(press 2)?")
+if c=="1":
+ tempchange()
+elif c=="2":
+ lengthchange()
+
 
 
                                            
